a842cc994405dabe7566937dab4620bdf440e126
Lolita <lolita454462@gmail.com>
2025-06-01 21:28:24 +0200

    logo link fixed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ const Logo = () => {
     return (
         <>
             <svg width="30" height="30">
-                <use href="../../../public/icons/symbol-defs.svg#icon-favicon-light"></use>
+                <use href="/public/icons/symbol-defs.svg#icon-favicon-light"></use>
             </svg>
         </>
     );
