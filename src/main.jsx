d7c8e7de8a2d02fd08057908aff64ab2ed893dcb
Lolita <lolita454462@gmail.com>
2025-07-19 11:21:44 +0200

    eslint/pluginkit update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import 'modern-normalize';
+import 'modern-normalize'; //?
 import './index.css';
 
 import App from './components/App.jsx';
